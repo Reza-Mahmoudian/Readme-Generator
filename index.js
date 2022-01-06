7746fe2299ba7253863967aebe8b2c65d0c101af
Reza Mahmoudian <reza@Rezas-MacBook-Pro.local>
2022-01-05 20:39:17 -0500

    some question added

--- index.js
+++ index.js
@@ -6,10 +6,24 @@ const generateMarkdown = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the Title of your Project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project?'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+
+}
 
 // TODO: Create a function to initialize app
 function init() {}
