6ba4f7c25000da222184cdebf980a234e24085c4
Reza Mahmoudian <reza@Rezas-MacBook-Pro.local>
2022-01-05 20:52:57 -0500

    adjusted index.js

--- index.js
+++ index.js
@@ -62,12 +62,16 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
 
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers)=>{
+        writeToFile("README.md", generateMarkdown(answers))
+    })
+}
 
 // Function call to initialize app
 init();
