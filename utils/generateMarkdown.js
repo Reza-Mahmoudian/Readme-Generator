cf4821350fb27647d70b63ea6d7f2d7a5b484b8e
Reza Mahmoudian <reza@Rezas-MacBook-Pro.local>
2022-01-05 20:58:48 -0500

    generateMarkdown.js adjusted

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,19 +7,62 @@ function renderLicenseBadge(license) {
   return `![Badge](https://img.shields.io/badge/license-${license}-blue)`
 }
 
-
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'None'){
+    return '';
+  } 
+  return `
+  * [License](#license)`
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None'){
+    return '';
+  } 
+  return `
+  ## License 
+  This project has a ${license} license.
+  `
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+ 
+ ## Description
+ 
+ ${data.description}
 
+ ## Table of Contents
+ * [Installation](#Installation)
+ * [Usage](#Usage)${renderLicenseLink(data.license)}
+ * [Contributing](#Contributing)
+ * [Tests](#Tests)
+ * [Questions](#Questions)
+ 
+ ## Installation
+ ${data.installation}
+
+ ## Usage
+ ${data.usage} 
+ ${renderLicenseSection(data.license)}
+ ## Contributing
+ ${data.contribute}
+
+ ## Tests
+ ${data.tests}
+
+ ## Questions
+ here is the link to my Github: [github.com/${data.github}](github.com/${data.github})
+ If you have any questions regarding the project or anything else you can contact me at:
+ 
+  Email: [${data.email}](${data.email}) 
 `;
 }
 
